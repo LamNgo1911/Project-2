b05778aebe6377720c21a9123503337d9fdc4976
Diddayngo <lamngo606@gmail.com>
2023-08-07 00:53:29 +0300

    fix app

--- backend/app.js
+++ backend/app.js
@@ -31,7 +31,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(cors());
+app.use(cors({ origin: allowedOrigin, credentials: true }));
 app.set("trust proxy", 1);
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(xss());

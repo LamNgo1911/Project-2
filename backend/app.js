d0b532ac91464e1345c2df4852d032df5009da3f
Diddayngo <lamngo606@gmail.com>
2023-08-09 17:19:13 +0300

    remove bugs

--- backend/app.js
+++ backend/app.js
@@ -31,7 +31,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.set("trust proxy", 1);
+
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(xss());
 app.use(mongoSanitize());

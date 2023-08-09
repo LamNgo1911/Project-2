84f85e9cb3779153e16ab7aa0d3995ca879c9f38
Diddayngo <lamngo606@gmail.com>
2023-08-09 19:07:29 +0300

    change loading

--- frontend/src/components/LoadingStart.jsx
+++ frontend/src/components/LoadingStart.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function LoadingStart() {
   return (
-    <div className='absolute top-0 left-0 w-full h-screen bg-black bg-opacity-50 z-50 flex justify-center items-center'>
+    <div className='absolute top-0 left-0 w-full h-screen bg-black bg-opacity-80 z-[9999] flex justify-center items-center'>
         <div className='animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-bgColorLight' />
       </div>
   )

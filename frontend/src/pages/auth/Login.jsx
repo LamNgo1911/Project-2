ad043232cfd7c5ac0a36efc9640280dbcd9d42aa
Diddayngo <lamngo606@gmail.com>
2023-08-09 19:05:59 +0300

    check data

--- frontend/src/pages/auth/Login.jsx
+++ frontend/src/pages/auth/Login.jsx
@@ -45,6 +45,7 @@ function Login() {
         navigate("/");
         window.location.reload(); // Refresh the page
       }
+      console("hello")
     } catch (error) {
       if (!error.response) {
         setError("No server response. Please try again later.");
